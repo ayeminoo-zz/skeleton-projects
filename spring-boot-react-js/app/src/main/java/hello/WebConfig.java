92b7aa285a1d123931b8635db4f7b31bebc4069b
ayeminoo <ayeminoosc@gmail.com>
2018-01-10 00:59:12 +0800

    integrated ui with spring

--- spring-boot-react-js/app/src/main/java/hello/WebConfig.java
+++ spring-boot-react-js/app/src/main/java/hello/WebConfig.java
@@ -1,7 +1,24 @@
 package hello;
 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 /**
  * Created by ayeminoo on 1/9/18.
  */
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations(
+                    "classpath:/META-INF/resources/webjars/");
+        }
+
+        if (!registry.hasMappingForPattern("/uiapp/**")) {
+            registry.addResourceHandler("/uiapp/**").addResourceLocations(
+                    "classpath:/META-INF/resources/uiapp/");
+        }
+    }
 }
